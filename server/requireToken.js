117d4d33ed596debe2f7709a4b3355fe858f314c
Peter Rifkind <prifkind@gmail.com>
2022-11-14 22:42:07 -0500

    Fixed a bug with requireToken model imports

--- server/requireToken.js
+++ server/requireToken.js
@@ -1,8 +1,10 @@
-const { User } = require("./db");
+const { models } = require("./db");
 
+const { User } = models;
 const requireToken = async (req, res, next) => {
   try {
     req.user = await User.findByToken(req.headers.authorization);
+    console.log("token found", req.user);
     next();
   } catch (err) {
     throw new Error("Bad credentials!");
