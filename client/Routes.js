8e15f923789875edd405c4d8dabc808b5a51669d
Allan Luo <allanluo@gmail.com>
2022-11-14 20:58:55 -0600

    started shoppingcart

--- client/Routes.js
+++ client/Routes.js
@@ -16,7 +16,7 @@ import Profile from "./components/UserProfile/Profile/Profile";
 import OrderHistory from "./components/UserProfile/Profile/OrderHistory/OrderHistory";
 import ViewCustomers from "./components/Admin/ViewCustomers";
 import EditProfile from "./components/UserProfile/Profile/EditProfile";
-import SingleProduct from "./components/Pages/SingleProduct";
+import SingleProduct from "./components/Pages/SingleProduct/SingleProduct.js";
 import { ManageProducts } from "./components/Admin/";
 
 const Routes = (props) => {
@@ -49,7 +49,7 @@ const Routes = (props) => {
           </Route>
           <Route
             exact
-            path="product/:id"
+            path="/product/:productId"
             render={(routeProps) => <SingleProduct {...routeProps} />}
           />
           <Route path="/checkout">
